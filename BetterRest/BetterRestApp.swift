0aa1ee776c51f6fbb639eaf44aa7f574657648ae
Tomek Mroziński <mrozoo@icloud.com>
2022-03-31 21:33:35 +0200

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Tomek on 31/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
